c9ae45b99131b7af9aa14b3bcea8998449ce91c4
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-09-20 06:18:57 -0700

    commit

---
+++ Day 03/homework/task2.py
@@ -0,0 +1,25 @@
+"""2) შექმენით მინიმუმ 5 ცვლადი, რომლებშიც შეინახავთ თქვენი ოჯახის წევრების სახელებს (მაგალითად: mother: "Tamo"), შემდეგ კი დაბეჭდავთ ერთ დიდ წინადადებაში (მაგალითად print("my mother is" + " " + mother))
+"""
+#step 1:ვქმნი 5 ცვლადს სადაც შევინახება ჩემი ოჯახის წევრების სახელებს 
+
+mother = "leila"
+
+father = "dato"
+
+brother = "sandro"
+
+myself = "nika"
+
+grandmother = "ana"
+
+#step 2:ცვლადების საშუალებით ვპრინტავთ წინადადებებს
+
+print("my mother is" + " " + mother)
+
+print("my father is" + " " + father)
+
+print("my grandmothers name  is" + " " + grandmother)
+
+print("my brothers name is" + " " + brother)
+
+print("my name is" + " " + myself)
