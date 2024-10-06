b8fef5dd2d781967ce49f632b539a66bbf4f0e18
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-10-06 11:39:59 -0700

    commit

---
+++ Day 04/homework/task2.py
@@ -0,0 +1,22 @@
+'''2) შეასწორეთ (debug) შემდეგი კოდი:
+
+name = "Alice"
+print(name)
+
+salary = 75000
+# age -> 30
+role = "Software Engineer"
+print("salary")
+# print(role)
+'''
+
+#უნდა შევასწოროთ კოდი 
+
+name = "Alice"
+print(name)
+
+salary = 75000
+age = 30
+role = "software Engineer"
+print(salary)
+print(role)
---
+++ Day 04/homework/task3.py
@@ -0,0 +1,39 @@
+'''
+3) შექმენით 3 ცვლადი რომლებშიც შეინახავთ რიცხვით integer მნიშვნელობებს, შემდეგ სამივეს შორის დაბეჭდეთ შემდეგი ოპერაციების (+,-,*,/) შედეგები
+'''
+
+Num1 = (44)
+
+Num2 =  (22)
+
+Num3 =  (11)
+
+
+print(Num1 + Num2)
+
+print(Num1 - Num2)
+
+print(Num1 * Num2)
+
+print(Num1 / Num2)
+
+print(Num1 + Num3)
+
+print(Num1 - Num3)
+
+print(Num1 * Num3)
+
+print(Num1 / Num3)
+
+print(Num2 + Num3)
+
+print(Num2 - Num3)
+
+print(Num2 * Num3)
+
+print(Num2 / Num3)
+
+
+
+
+
