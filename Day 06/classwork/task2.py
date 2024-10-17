4d4bf94d638ba7758df98feee0d964ea274574ec
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-10-17 07:15:37 -0700

    commit

---
+++ Day 06/classwork/task2.py
@@ -0,0 +1,32 @@
+'''ცვლადების გამოყენებით დაბეჭდეთ თქვენი სახელი და გვარი, შემდეგ დაბეჭდეთ 3 მათემატიკური ოპერაციის (+,-,*) შედეგი და თქვენი ჰობი, ყველაფერი ახსენით კომენტარებით ქართულად და ცვლადებს სახელები დაარქვით snake_case-ის სტილში (გამოიყენეთ სტანდარტები და საუკეთესო პრაქტიკები)'''
+
+
+#Declaring a variables
+
+Full_Name = "Nikoloz Kakabadze"
+
+#Displaying a variable
+
+print(Full_Name)
+
+#printing math operations 
+
+print(1+1)
+print(1+2)
+print(1+3)
+
+print(5-2)
+print(5-3)
+print(5-4)
+
+print(1*1)
+print(4*9)
+print(5*5)
+
+#Declaring a variable
+
+my_hobby = "programming"
+
+#Displaying a variable
+
+print(my_hobby)
---
+++ Day 06/homework/task2.py
@@ -0,0 +1,44 @@
+'''2) შექმენით 5 ცვლადი რომლებშიც შეინახავთ წიგნების თავდაპირველ ფასს, შემდეგ შექმენით ცვლადი რომელშიც შეინახავთ ფასდაკლების ოდენობას, შექმენით ახალი ფასის მქონე წიგნების ცვლადები, რომელთა მნიშვნელობა იქნება ძველ მნიშვნელობას გამოკლებული ახალი, საბოლოოდ კი დაბეჭდეთ ახალი წიგნების ფასები (გამოიყენეთ დღეს ნასწავლი მასალა და საუკეთესო პრაქტიკები: გამოიყენეთ ის რომ ცვლადის მნიშვნელობის მინიჭებისას შეგიძლიათ სხვა ცვლადის გამოყენება, კოდი ახსენით კომენტარების საშვალებით, ცვლადებს დაარქვით აღმწერი სახელები snake_case-ის სტილში)'''
+
+#Declaring a variables book prices
+
+book_price = "7"
+
+Book_price = "9"
+
+Book_Price = "18"
+
+BOOK_price = "22"
+
+BOOK_PRICE = "30"
+
+discount = "4"
+
+#Declaring a variable Create new price book variables whose value will be the old value minus the new
+
+new_book_price = book_price - discount
+
+new_Book_price = Book_price - discount
+
+new_Book_Price = Book_Price - discount 
+
+new_BOOK_price = BOOK_price - discount
+
+new_BOOK_PRICE = BOOK_PRICE - discount
+
+
+
+#Declaring a variable book new  prices
+
+new_book_price = "3"
+
+new_Book_price = "6"
+
+new_Book_Price= "14"
+
+new_BOOK_price = "18"
+
+new_BOOK_PRICE = "26"
+
+
+
