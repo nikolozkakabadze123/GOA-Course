859b1ede3647f1fdb5f2853f385696c6fc45ad2a
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-10-31 15:38:20 +0400

    commit

---
+++ Day 08/homework/task4.py
@@ -0,0 +1,47 @@
+
+'''4) შექმენით 10 string ტიპის და 10 ინტეჯერ ტიპის ცვლადი, გააკეთეთ 5 კონკატინაციისა (სტრინგების შეერთების) და 5 მათემატიკური ჯამის მაგალითი'''
+
+
+#10 string ტიპის ცვლადი
+string1 = "hello"
+string2 = "hey"
+string3 = "georgia"
+string4 = "how are you"
+string5 = "what"
+string6 = "playstation"
+string7 = "walmart"
+string8 = "the best"
+string9 = "print"
+string10 = "programming"
+
+
+#10 ინტეჯერ ტიპის ცვლადი
+int1 = 5
+int2 = 10
+int3 = 15
+int4 = 20
+int5 = 25
+int6 = 30
+int7 = 35
+int8 = 40
+int9 = 45
+int10 = 50
+
+
+#5 კონკატინაციის ცვლადი 
+concat1 = string1 + " " + string2 
+concat2 = string3 + " " + string4 
+concat3 = string5 + " " + string6  
+concat4 = string7 + " " + string8 
+concat5 = string9 + " " + string10  
+
+
+#5 მათემატიკური ჯამის მაგალითი
+sum1 = int1 + int2  
+sum2 = int3 + int4  
+sum3 = int5 + int6  
+sum4 = int7 + int8 
+sum5 = int9 + int10  
+
+
+
