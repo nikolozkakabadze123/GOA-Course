51eb90299c74263c00f14c4ea1b9aedf5111a95a
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-11-09 16:50:16 +0400

    commit

--- Day 10/homework/task5.py
+++ Day 10/homework/task5.py
@@ -2,6 +2,12 @@
 5) აიღეთ 2 ცვლადი, რომლებშიც გექნებათ რიცხვითი integer ტიპის მნიშვნელობები, შემდეგ აიღეთ მესამე ცვლადი რომელშიც შეინახავთ უდიდესი მნიშვნელობის ცვლადის გაყოფას უმცირესზე, საბოლოოდ type ინსტრუქციის გამოყენებით შეამოწმეთ 
 მე-3 ცვლადის მონაცემის ტიპი.
 '''
-
+#we take two variables and then divide the largest variable by the smallest
 num1 = 9
 num2 = 3
+print(num1 / num2)
+answer = 3.0
+
+#after we divided the variables we need to get data type of an answer 
+print(type(answer)) 
+
