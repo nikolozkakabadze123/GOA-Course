cfb769521107e50715565142d41672a20f81bb18
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-11-21 17:17:17 +0400

    commit

---
+++ Day 12/classwork/task3.py
@@ -0,0 +1,9 @@
+'''3) მომხმარებელს შემოატანინეთ ასაკი,დაუმატეთ 5 და შემდეგ დაუბეჭდეთ წინადადება ასეთ ფორმატში: 'You will be <ასაკი> years old'''
+
+age = int(input("enter your age : "))
+
+age2 = age + 5
+
+print("You will be " + str(age2) + " years old")
+
+    
