d9192e69539ce85fee2b02ac20bbcbe9ee1c824d
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-12-26 17:26:51 +0400

    commit

---
+++ Day 18/classwork/task2.py
@@ -0,0 +1,15 @@
+#2)გააკეთეთ 5 მაგალითი ლოგიკურ ოპერატორებზე , ხოლო 5 მაგალითი შედარების ოპერატორებზე
+
+#ლოგიკურ ოპერატორებზე
+True and True #-> True
+True and False #-> False 
+True or True #-> True
+True or False #-> True
+False or False #-> False
+
+#შედარებით ოპერატორებზე
+10 = 10 #->True
+11 >= 9 #->True
+3 < 12 #->True
+5 > 8 #->False
+12 <= 2 #->False
